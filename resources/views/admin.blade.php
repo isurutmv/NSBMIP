5044421afb09a47aed985196f31e813737915f15
isurutmv <isurutmv@gmail.com>
2018-04-28 17:15:10 +0530

    change admin template

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,18 +10,18 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-    {{--@if(getenv('APP_ENV')=='local')--}}
-        {{--<link rel="stylesheet" href="{{ URL::to('/') }}/assets/bootstrap/css/bootstrap.min.css">--}}
-        {{--<link rel="stylesheet" href="{{ URL::to('/') }}/assets/font-awesome/css/font-awesome.min.css">--}}
-        {{--<link rel="stylesheet" href="{{ URL::to('/') }}/assets/bootstrap-datepicker/bootstrap-datepicker3.min.css">--}}
+    @if(getenv('APP_ENV')=='local')
+        <link rel="stylesheet" href="{{ URL::to('/') }}/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ URL::to('/') }}/assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ URL::to('/') }}/assets/bootstrap-datepicker/bootstrap-datepicker3.min.css">
 
-    {{--@else--}}
+    @else
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
               integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw=="
               crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker3.css">
-    {{--@endif--}}
+    @endif
 
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ URL::to('/') }}/assets/theme/dist/css/AdminLTE.min.css">
