8c5c450fdd515064ac8dba4dde19a9e5dd87f21e
isurutmv <isurutmv@gmail.com>
2018-04-30 12:24:06 +0530

    change twak position232

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -41,20 +41,20 @@
         @include('admin.templates.header')
         @include('admin.templates.left-sidebar')
         <div class="content-wrapper">
-            <div id='tawk_59dd97b6c28eca75e462555a'></div>
-            <!--Start of Tawk.to Script-->
-            <script type="text/javascript">
-                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_59dd97b6c28eca75e462555a';
-                (function(){
-                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                    s1.async=true;
-                    s1.src='https://embed.tawk.to/59dd97b6c28eca75e462555a/1ccak4nim';
-                    s1.charset='UTF-8';
-                    s1.setAttribute('crossorigin','*');
-                    s0.parentNode.insertBefore(s1,s0);})();
-            </script>
-            <!--End of Tawk.to Script-->
             @section('content')
+                <div id='tawk_59dd97b6c28eca75e462555a'></div>
+                <!--Start of Tawk.to Script-->
+                <script type="text/javascript">
+                    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_59dd97b6c28eca75e462555a';
+                    (function(){
+                        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                        s1.async=true;
+                        s1.src='https://embed.tawk.to/59dd97b6c28eca75e462555a/1ccak4nim';
+                        s1.charset='UTF-8';
+                        s1.setAttribute('crossorigin','*');
+                        s0.parentNode.insertBefore(s1,s0);})();
+                </script>
+                <!--End of Tawk.to Script-->
             @show
 
         </div>
